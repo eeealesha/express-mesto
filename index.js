4d9142509d9c4f3de243dfac22516c9c3b6bc5b7
Alexey Gavrilov <54736886+eeealesha@users.noreply.github.com>
2020-12-20 13:50:13 +0300

    mongoDB connected

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const app = express();
 const path = require('path');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
+const mongoose = require('mongoose');
 const PORT = 3000;
+
+mongoose.connect('mongodb://localhost:27017/mestodb',{
+    useNewUrlParser:true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
 
 app.use(express.static(path.join(__dirname, 'public')))
 
