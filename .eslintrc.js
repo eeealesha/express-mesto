17adc2783c8efd09f0fcf4b6c3b2406d658fcbb4
Alexey Gavrilov <54736886+eeealesha@users.noreply.github.com>
2020-12-21 10:21:51 +0300

    es lint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,15 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "plugin:react/recommended",
-        "airbnb"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-    ],
-    "rules": {
-      "no-underscore-dangle": 'off'
-    }
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+  },
 };
